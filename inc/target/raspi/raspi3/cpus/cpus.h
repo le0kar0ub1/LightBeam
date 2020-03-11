2424eb18710a63a12a054e9b8248a028881612b3
leo.karoubi <leokaroubi@gmail.com>
2020-03-11 10:39:38 +0100

    cpus id

---
+++ inc/target/raspi/raspi3/cpus/cpus.h
@@ -0,0 +1,9 @@
+#ifndef __CPUS_H_
+#define __CPUS_H_
+
+#include "def/typedef.h"
+
+int cpu_getid(void);
+void cpu_wait(void);
+
+#endif
---
+++ src/target/raspi/raspi3/cpus/scheduler.c
@@ -0,0 +1,6 @@
+#include "target/raspi/raspi3/cpus/cpus.h"
+
+void cpu_wait(void)
+{
+
+}
