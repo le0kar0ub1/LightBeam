48d502551e3391fa85022b17dc65cbb9cbf9bc56
leo.karoubi <leokaroubi@gmail.com>
2020-04-05 01:00:03 +0200

    init branch

--- mkhelper/arch.mk
+++ mkhelper/arch.mk
@@ -3,11 +3,15 @@
  #
 
 # Used as target
-export HANDLED_PLATEFORM	:=	raspi/raspi3
+export HANDLED_PLATEFORM	:=	raspi/raspi3	\
+								x86/i386		\
+								x86/x86_64
 
 
 ## For information #
-export HANDLED_PROC		:=	cortex-A53
+export HANDLED_PROC		:=	cortex-A53	\
+							i386		\
+							x86_64
 
 
 # Just a List
