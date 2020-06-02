5ff8b921d80179124fe8536fa77e9534b53d72dd
leo.karoubi <leokaroubi@gmail.com>
2020-06-02 17:30:48 +0200

    interfacing arch/non-arch cpus functions

--- inc/kernel/cpus/cpus.h
+++ inc/kernel/cpus/cpus.h
@@ -7,6 +7,7 @@
 
 typedef int cpuid_t;
 
+#ifdef KCONFIG_MAXCPUS
 enum CPU_STATE
 {
     CPU_IS_UNDEFINED  = 0,
@@ -28,19 +29,24 @@ struct cpustate_t
     } routine;
 };
 
-#define cpu_log(x, ...) (arch_cpu_log("[CPU %d][LOG]: %s\n", arch_cpu_get_id(), x, ##__VA_ARGS__))
-#define cpu_wrn(x, ...) (arch_cpu_wrn("[CPU %d][WRN]: %s\n", arch_cpu_get_id(), x, ##__VA_ARGS__))
-#define cpu_err(x, ...) (arch_cpu_err("[CPU %d][ERR]: %s\n", arch_cpu_get_id(), x, ##__VA_ARGS__))
-
-void arch_cpu_scheduler(void);
 void arch_cpu_log(char const *fmt, ...);
 void arch_cpu_wrn(char const *fmt, ...);
 void arch_cpu_err(char const *fmt, ...);
 void arch_cpu_halt(void);
 void arch_cpu_trigger_cpu(cpuid_t core);
 void arch_cpu_acquire_trigger_cpu(void);
-
 cpuid_t arch_cpu_get_id(void);
+
+#define cpu_log(x, ...) (arch_cpu_log("[CPU %d][LOG]: %s\n", arch_cpu_get_id(), x, ##__VA_ARGS__))
+#define cpu_wrn(x, ...) (arch_cpu_wrn("[CPU %d][WRN]: %s\n", arch_cpu_get_id(), x, ##__VA_ARGS__))
+#define cpu_err(x, ...) (arch_cpu_err("[CPU %d][ERR]: %s\n", arch_cpu_get_id(), x, ##__VA_ARGS__))
+#define cpu_halt()                arch_cpu_halt()
+#define cpu_trigger_cpu(x)        arch_cpu_trigger_cpu(x)
+#define cpu_acquire_trigger_cpu() arch_cpu_acquire_trigger_cpu()
+#define cpu_get_id()              arch_cpu_get_id()
+
+void arch_cpu_scheduler(void);
+void cpu_scheduler(void);
 
 
 void cpu_set_state(cpuid_t core, enum CPU_STATE state);
@@ -48,8 +54,8 @@ enum CPU_STATE cpu_get_state(cpuid_t core);
 void cpu_set_routine(int core, int (*routine)(int, char **), int argc, char **argv);
 struct cpuroutine_t *cpu_get_routine(cpuid_t core);
 
-void cpu_scheduler(void);
 void cpu_exec_routine(int core, int (*routine)(int, char **), int argc, char **argv);
+#endif
 
 void __deadloop(void);
 void __hang(void);
--- inc/target/riscv/riscv64/sifive/cpus/cpus.h
+++ inc/target/riscv/riscv64/sifive/cpus/cpus.h
@@ -1,9 +1,6 @@
 #ifndef __CPUS_H_
 #define __CPUS_H_
 
-#include "kernel/def/typedef.h"
-#include "kernel/def/keyword.h"
-#include "kernel/schedule/semaphore.h"
 #include "kernel/cpus/cpus.h"
 
 #endif
